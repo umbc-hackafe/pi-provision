ebdfacddb3060524c57a66564f0ac39d78a6a2a9
Dylan Whichard <dylan@whichard.com>
2015-04-26 21:31:21 -0400

    Change messages

--- pi-provision.sh
+++ pi-provision.sh
@@ -87,9 +87,10 @@ sync
 eject $DEV || true
 
 echo "DONE -- Boot up Pi and connect to network"
+echo "Waiting for SSH on $PIHOST..."
 
 while true; do curl -s $PIHOST:22 | grep -qi ssh && break; sleep 5; done
 
-echo "Prepare to run 'salt $PIHOST. state.highstate' on the salt master"
+echo "Connecting... Please enter 'root':"
 
 ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@$PIHOST 'pacman --noconfirm -Syu;pacman --noconfirm -S salt-zmq; salt-call state.highstate -l debug'
